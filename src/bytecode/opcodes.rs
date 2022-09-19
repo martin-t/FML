9d15836d2aa50dbb63a555cba466f72138d6a8ec
Martin Taibr <taibr.martin@gmail.com>
2022-09-19 19:20:00 +0200

    Accidentally a word

--- src/bytecode/opcodes.rs
+++ src/bytecode/opcodes.rs
@@ -30,7 +30,7 @@ pub enum OpCode {
      * [ProgramObject::Integer]: ../objects/enum.ProgramObject.html#variant.Integer
      * [ProgramObject::Null]: ../objects/enum.ProgramObject.html#variant.Null
      */
-    // LATER a type safe way to ConstantPoolIndex to the right constant type?
+    // LATER A type safe way to limit ConstantPoolIndex to the right constant type(s)?
     Literal {
         /// Integer|Null|Boolean
         index: ConstantPoolIndex,
