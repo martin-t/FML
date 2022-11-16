789157a4d6ea901533071515df1cdf51574dcf18
Martin Taibr <taibr.martin@gmail.com>
2022-11-16 23:57:36 +0100

    Remove commented out code

--- src/bytecode/state.rs
+++ src/bytecode/state.rs
@@ -8,6 +8,7 @@ use crate::bytecode::program::*;
 use crate::bytecode::heap::*;
 
 #[derive(Eq, PartialEq, Ord, PartialOrd, Debug, Clone, Copy)]
+// LATER(martin-t) It should be possible to avoid the Option here.
 pub struct InstructionPointer(Option<Address>);
 impl InstructionPointer {
     pub fn new() -> Self { InstructionPointer(None) }
@@ -213,24 +214,7 @@ pub struct State {
     pub heap: Heap
 }
 
-// pub struct State {
-//     pub instruction_pointer: Option<Address>,
-//     pub frames: Vec<LocalFrame>,
-//     pub operands: Vec<Pointer>,
-//     pub globals: HashMap<String, Pointer>,
-//     pub functions: HashMap<String, ProgramObject>,
-//     pub memory: Heap,
-// }
-
 impl State {
-    // pub fn set_heap_size (mut self, heap_size: usize) -> Self {
-    //     self.heap.set_size(heap_size);
-    //     self
-    // }
-    // pub fn set_heap_log(mut self, heap_log: PathBuf) -> Self {
-    //     self.heap.set_log(heap_log);
-    //     self
-    // }
     pub fn from(program: &Program) -> Result<Self> {                                                // LATER(kondziu) error handling is a right mess here.
 
         let entry_index = program.entry.get()
@@ -307,184 +291,7 @@ impl State {
             heap: Heap::new()
         }
     }
-
-    // pub fn bump_instruction_pointer(&mut self, program: &Program) -> &Option<Address> {
-    //     let address = program.code().next_address(self.instruction_pointer);
-    //     self.instruction_pointer = address;
-    //     &self.instruction_pointer
-    // }
-
-    // pub fn set_instruction_pointer(&mut self, address: Option<Address>) -> () {
-    //     self.instruction_pointer = address;
-    // }
-    //
-    // pub fn has_next_instruction_pointer(&mut self) -> bool {
-    //     self.instruction_pointer != None
-    // }
-
-    // pub fn current_frame(&self) -> Option<&LocalFrame> {
-    //     self.frames.last()
-    // }
-    //
-    // pub fn current_frame_mut(&mut self, ) -> Option<&mut LocalFrame> {
-    //     self.frames.last_mut()
-    // }
-    //
-    // pub fn pop_frame(&mut self) -> Option<LocalFrame> {
-    //     self.frames.pop()
-    // }
-
-    // pub fn new_frame(&mut self, return_address: Option<Address>, slots: Vec<Pointer>, ) {
-    //     self.frames.push(LocalFrame { locals: slots, return_address });
-    // }
-    //
-    // pub fn peek_operand(&mut self) -> Option<&Pointer> {
-    //     self.operands.last()
-    // }
-
-    // pub fn pop_operand(&mut self) -> Option<Pointer> {
-    //     self.operands.pop()
-    // }
-    //
-    // pub fn push_operand(&mut self, object: Pointer) {
-    //     self.operands.push(object)
-    // }
-    //
-    // pub fn allocate_and_push_operand(&mut self, object: HeapObject) {
-    //     self.operands.push(Pointer::from(self.memory.allocate(object)))
-    // }
-    //
-    // pub fn get_function(&self, name: &str) -> Option<&ProgramObject> {
-    //     self.functions.get(name)
-    // }
-    //
-    // pub fn get_global(&self, name: &str) -> Option<&Pointer> {
-    //     self.globals.get(name)
-    // }
-
-    // #[allow(dead_code)]
-    // pub fn register_global(&mut self, name: String, object: Pointer) -> Result<(), String> {
-    //     if self.globals.contains_key(&name) {
-    //         Err(format!("Global {} already registered (with value {:?})",
-    //                     &name, self.globals.get(&name).unwrap()))
-    //     } else {
-    //         self.globals.insert(name, object);
-    //         Ok(())
-    //     }
-    // }
-
-    // #[allow(dead_code)]
-    // pub fn allocate_and_register_global(&mut self, name: String, object: HeapObject) -> Result<(), String> {
-    //     let pointer = self.allocate(object);
-    //     self.register_global(name, pointer)
-    // }
-
-    // pub fn update_global(&mut self, name: String, object: Pointer) {
-    //     self.globals.insert(name, object);
-    // }
-
-    // pub fn set_instruction_pointer_from_label(&mut self, program: &Program, name: &str) -> Result<(), String> {
-    //     match program.get_label(name) {
-    //         None => Err(format!("Label {} does not exist", name)),
-    //         Some(address) => {
-    //             self.instruction_pointer = Some(*address);
-    //             Ok(())
-    //         }
-    //     }
-    // }
-
-    // #[allow(dead_code)]
-    // pub fn push_global_to_operand_stack(&mut self, name: &str) -> Result<(), String> {
-    //     let global = self.get_global(name).map(|e| e.clone());
-    //     match global {
-    //         Some(global) => {
-    //             self.push_operand(global);
-    //             Ok(())
-    //         },
-    //         None => {
-    //             Err(format!("No such global {}", name))
-    //         }
-    //     }
-    // }
-
-    // pub fn dereference_to_string(&self, pointer: &Pointer) -> String {
-    //     self.memory.dereference_to_string(pointer)
-    // }
-    //
-    // pub fn dereference_mut(&mut self, pointer: &HeapIndex) -> Option<&mut HeapObject> {
-    //     self.memory.dereference_mut(pointer)
-    // }
-    //
-    // pub fn dereference(&self, pointer: &HeapIndex) -> Option<&HeapObject> {
-    //     self.memory.dereference(pointer)
-    // }
-    //
-    // pub fn allocate(&mut self, object: HeapObject) -> Pointer {
-    //     Pointer::from(self.memory.allocate(object))
-    // }
-
-    // pub fn copy_memory(&mut self, pointer: &Pointer) -> Option<Pointer> {
-    //     match pointer {
-    //         Pointer::Reference(p) =>
-    //             self.memory.copy(p).map(|p| Pointer::from(p)),
-    //         tagged_pointer =>
-    //             Some(tagged_pointer.clone())
-    //     }
-    // }
 }
-
-
-// #[derive(PartialEq,Debug)]
-// pub struct LocalFrame {
-//     locals: Vec<Pointer>, /* ProgramObject::Slot */
-//     return_address: Option<Address>, /* address */
-// }
-//
-// impl LocalFrame {
-//     pub fn empty() -> Self {
-//         LocalFrame {
-//             locals: vec!(),
-//             return_address: None,
-//         }
-//     }
-//
-//     #[allow(dead_code)]
-//     pub fn from(return_address: Option<Address>, slots: Vec<Pointer>) -> Self {
-//         LocalFrame {
-//             return_address,
-//             locals: slots,
-//         }
-//     }
-//
-//     pub fn return_address(&self) -> &Option<Address> {
-//         &self.return_address
-//     }
-//
-//     pub fn get_local(&self, index: &LocalFrameIndex) -> Option<Pointer> {
-//         match index.value() {
-//             index if index as usize >= self.locals.len() => None,
-//             index => Some(self.locals[index as usize].clone()), // new ref
-//         }
-//     }
-//
-//     pub fn update_local(&mut self, index: &LocalFrameIndex, local: Pointer) -> Result<(), String> {
-//         match index.value() {
-//             index if index as usize >= self.locals.len() =>
-//                 Err(format!("No local at index {} in frame", index)),
-//             index => {
-//                 self.locals[index as usize] = local;
-//                 Ok(())
-//             },
-//         }
-//     }
-//
-//     #[allow(dead_code)]
-//     pub fn push_local(&mut self, local: Pointer) -> LocalFrameIndex {
-//         self.locals.push(local);
-//         assert!(self.locals.len() <= 65_535usize);
-//         LocalFrameIndex::new(self.locals.len() as u16 - 1u16)
-//     }
-// }
 
 pub struct Output();
 
