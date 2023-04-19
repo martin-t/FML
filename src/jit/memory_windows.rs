1841a745aea2ad1dd052939b0952edd59c98da32
Martin Taibr <taibr.martin@gmail.com>
2023-04-19 03:37:07 +0200

    Yolo

--- src/jit/memory_windows.rs
+++ src/jit/memory_windows.rs
@@ -1,11 +1,29 @@
+//! Completely untested executable memory for Windows.
+//!
+//! LATER(martin-t) Test on Windows.
+
+use windows_sys::Win32::System::Memory::{VirtualProtect, PAGE_EXECUTE_READWRITE};
+
 pub struct JitMemory {
     pub code: *mut u8,
-    size: usize,
 }
 
 impl JitMemory {
-    #[allow(unused_variables)] // LATER(martin-t) Remove
     pub fn new(data: &[u8]) -> Self {
-        unimplemented!("Windows is currently not supported")
+        let vec = data.to_vec();
+        let size = vec.len();
+        let code = vec.leak().as_mut_ptr();
+        let mut old_prot = 0u32;
+        let ret = unsafe { VirtualProtect(code as *mut _, size, PAGE_EXECUTE_READWRITE, &mut old_prot as *mut _) };
+        assert_ne!(ret, 0);
+        Self { code }
+    }
+}
+
+impl Drop for JitMemory {
+    fn drop(&mut self) {
+        // LATER(martin-t) Currently we're leaking the data.
+        //  Undo VirtualProtext, then drop the Vec.
+        //  Will probably need to use from_raw_parts which is not stable yet.
     }
 }
